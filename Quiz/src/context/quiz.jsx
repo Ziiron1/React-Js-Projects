263951f22d5bb173be15d1da5b4a2a6a99247bec
Ziiron1 <guilherme_profissional@yahoo.com.br>
2022-12-02 11:31:11 -0300

    Organizing Folders

--- Quiz/src/context/quiz.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const QuizContext = createContext();
-
-export const QuizProvider = ({ children }) => {
-  const value = { name: "Quiz" };
-
-  return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
-};
