95827bd3818ceed24034e75df663f3346d8e9f72
Ziiron1 <guilherme_profissional@yahoo.com.br>
2023-01-04 12:00:07 -0300

    Finishing TictacToe game

--- TictacToe/src/App.jsx
+++ TictacToe/src/App.jsx
@@ -3,18 +3,18 @@ import './styles.scss';
 
 const winningCombinations = [
   // Horizontal
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
+  { indexes: [0, 1, 2], orientation: 'horizontal' },
+  { indexes: [3, 4, 5], orientation: 'horizontal' },
+  { indexes: [6, 7, 8], orientation: 'horizontal' },
 
   // Vertical
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
+  { indexes: [0, 3, 6], orientation: 'vertical' },
+  { indexes: [1, 4, 7], orientation: 'vertical' },
+  { indexes: [2, 5, 8], orientation: 'vertical' },
 
   // Diagonal
-  [0, 4, 8],
-  [2, 4, 6],
+  { indexes: [0, 4, 8], orientation: 'diagonal-1' },
+  { indexes: [2, 4, 6], orientation: 'diagonal-2' },
 ]
 
 
@@ -48,6 +48,7 @@ function App() {
 
   useEffect(() => {
     checkWinner();
+    checkGameEnded();
   }, [gameData]);
 
   useEffect(() => {
@@ -56,28 +57,37 @@ function App() {
     }
   }, [winningCombo]);
 
+
+  const checkGameEnded = () => {
+    if (gameData.every((item) => item !== 0)) {
+      alert('Jogo deu Velha!')
+    }
+  }
+
+
   const checkWinner = () => {
     let winner = null;
 
-    for (let values of winningCombinations) {
-      console.log(values[0]);
+    for (let combination of winningCombinations) {
+      const { indexes } = combination;
+      console.log(indexes[0]);
       if (
-        gameData[values[0]] === 1 &&
-        gameData[values[1]] === 1 &&
-        gameData[values[2]] === 1
+        gameData[indexes[0]] === 1 &&
+        gameData[indexes[1]] === 1 &&
+        gameData[indexes[2]] === 1
       ) {
         winner = 'player 1'
       }
 
       if (
-        gameData[values[0]] === 2 &&
-        gameData[values[1]] === 2 &&
-        gameData[values[2]] === 2
+        gameData[indexes[0]] === 2 &&
+        gameData[indexes[1]] === 2 &&
+        gameData[indexes[2]] === 2
       ) {
         winner = 'player 2'
       }
       if (winner) {
-        setWinningCombo(values);
+        setWinningCombo(combination);
         break;
       }
     }
@@ -94,6 +104,10 @@ function App() {
               handleClick(index);
             }}
             key={index}
+            className={
+              winningCombo?.indexes.includes(index)
+                ? winningCombo.orientation
+                : undefined}
           >
             <abbr title="">{index}</abbr>
             {value === 1 && '❌'}
